380aea651a662eb7b90cbef17982deccd187a9e6
ภูรินทร์ นิกพงศ์ <123phoorin@gmail.com>
2023-01-03 23:13:31 +0700

    สร้าง api userResult (Mock)

--- server-ts/src/api/index.ts
+++ server-ts/src/api/index.ts
@@ -1,5 +1,6 @@
 import Router from 'koa-router'
 import announcement from './announcement'
+import userResult from './user_result'
 
 const apiRouter = new Router()
 
@@ -7,7 +8,7 @@ apiRouter.get('/api/greet', (ctx,next) => {
     ctx.body = {msg: 'Hello world'}
 })
 apiRouter.use('/api/announcement',announcement.routes())
-
+apiRouter.use('/api/userResult',userResult.routes())
 export default apiRouter
 
 
