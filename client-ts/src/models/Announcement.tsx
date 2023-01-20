6f7415c9797e4564a8f48512eed6f6bf81bf3ddd
ภูรินทร์ นิกพงศ์ <123phoorin@gmail.com>
2023-01-20 13:49:18 +0700

    เพิ่ม model announcement and userResult

---
+++ client-ts/src/models/Announcement.tsx
@@ -0,0 +1,10 @@
+
+export default interface Announcement{
+    id: number
+    topic: string
+    description: string
+    remarkIfPositive: string
+    remarkIfNegative: string
+    pubDateTime: Date
+    userCode: string
+  }
